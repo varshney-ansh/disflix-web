d9daa6193d90e55bfd700d5dbeadf61d5acc968c
Anshvar1 <anshvar22@gmail.com>
2024-04-26 18:11:40 +0530

    sitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,82 @@
+export default function sitemap() {
+    return [
+      {
+        url: 'https://disflix.in',
+        lastModified: new Date(),
+        changeFrequency: 'yearly',
+        priority: 1,
+      },
+      {
+        url: 'https://disflix.in/legal/privacy/',
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 0.8,
+      },
+      {
+        url: 'https://disflix.in/legal/terms',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.5,
+      },
+      {
+        url: 'https://disflix.in/legal/usage',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.5,
+      },
+      {
+        url: 'https://disflix.in/legal/notices',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.5,
+      },
+      {
+        url: 'https://disflix.in/legal/cookies',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.5,
+      },
+      {
+        url: 'https://disflix.in/legal/content',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.5,
+      },
+      {
+        url: 'https://disflix.in/help/about',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.5,
+      },
+      {
+        url: 'https://disflix.in/help/contact',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.5,
+      },
+      {
+        url: 'https://disflix.in/browse/anime',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.9,
+      },
+      {
+        url: 'https://disflix.in/browse/movies',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.9,
+      },
+      {
+        url: 'https://disflix.in/subscription',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.9,
+      },
+      {
+        url: 'https://disflix.in/mystuff',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.6,
+      },
+    ]
+  }
