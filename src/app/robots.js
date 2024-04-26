d467d8ccd50afde23642541a3659c22dcdbb9bda
Anshvar1 <anshvar22@gmail.com>
2024-04-26 17:19:59 +0530

    sitemap

---
+++ src/app/robots.js
@@ -0,0 +1,10 @@
+export default function robots() {
+    return {
+      rules: {
+        userAgent: '*',
+        allow: '/',
+        disallow: '',
+      },
+      sitemap: 'https://disflix.in/sitemap.xml',
+    }
+  }
