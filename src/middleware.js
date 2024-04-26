522ef11d192ae9dfe80f8ae44c366e8bb4ed9541
Anshvar1 <anshvar22@gmail.com>
2024-04-26 17:23:33 +0530

    tr

--- src/middleware.js
+++ src/middleware.js
@@ -1,6 +1,6 @@
 export { default } from 'next-auth/middleware';
 
 export const config = {
-    matcher: ["/subscription/:path*", "/watch/:path*", "/settings/:path*", "/detail/:path*", "/series/:path*", "/mystuff/:path*"]
+    matcher: ["/subscription/:path*", "/watch/:path*", "/settings/:path*", "/mystuff/:path*"]
 }
 
