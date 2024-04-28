aaf0f64d11b559e97af23fa986608d1cffac80b7
Anshvar1 <anshvar22@gmail.com>
2024-04-28 21:18:42 +0530

    fixed

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -27,6 +27,7 @@ const HomePage = async() => {
     return (
         <>
             <Navbar />
+            <h1 style={{display: 'none'}}>Disflix India | Enjoy Kung Fu Panda, Your Name, The Family Star and other Disflix Originals, popular movies, and hit TV shows — all available with your Disflix Membership. </h1>
             <div className={styles.container} style={{ background: '#00050D' }}>
                 <HomeCarousol movie1={movieSearch1} movie2={movieSearch2} movie3={movieSearch3}/>
                 <div style={{marginTop: "50px"}}>
